5d095a3dd21cc9f9e794df713a8bcddf56f09abb
cold-thunder <mdhemal202000@gmail.com>
2024-09-16 19:34:16 +0600

    advance class is done

--- lib/advance_anim/customPaintClass.dart
+++ lib/advance_anim/customPaintClass.dart
@@ -15,17 +15,20 @@ class _StateCustomPaintClass extends State<CustomPaintClass> with TickerProvider
   late Animation _animScale;
   late AnimationController _animScaleCont;
 
+  late Animation _animSides;
+  late AnimationController _animContSides;
+
   @override
   void initState(){
     super.initState();
     try{
-      _animContRot =  AnimationController(vsync: this, duration: const Duration(seconds: 2));
+      _animContRot =  AnimationController(vsync: this, duration: const Duration(seconds: 3));
       _animRot = Tween(begin: 0.0, end: 2*pi).animate(_animContRot);
       _animContRot.forward();
       _animContRot.repeat();
 
-      _animScaleCont = AnimationController(vsync: this, duration: const Duration(seconds: 2));
-      _animScale = Tween(begin: 0.0, end: 1).animate(
+      _animScaleCont = AnimationController(vsync: this, duration: const Duration(seconds: 3));
+      _animScale = Tween(begin: 50.0, end: 350.0).animate(
         CurvedAnimation(
           parent: _animScaleCont,
           curve: Curves.bounceInOut
@@ -33,6 +36,11 @@ class _StateCustomPaintClass extends State<CustomPaintClass> with TickerProvider
       );
       _animScaleCont.forward();
       _animScaleCont.repeat(reverse: true);
+
+      _animContSides = AnimationController(vsync: this, duration: const Duration(seconds: 3));
+      _animSides = IntTween(begin: 3, end: 10).animate(_animContSides);
+      _animContSides.forward();
+      _animContSides.repeat(reverse: true);
     }catch(err){
       print(err);
     }
@@ -41,6 +49,8 @@ class _StateCustomPaintClass extends State<CustomPaintClass> with TickerProvider
   @override
   void dispose(){
     _animContRot.dispose();
+    _animContSides.dispose();
+    _animScaleCont.dispose();
 
     super.dispose();
   }
@@ -67,20 +77,23 @@ class _StateCustomPaintClass extends State<CustomPaintClass> with TickerProvider
                   children: [
                     AnimatedBuilder(
                         animation: Listenable.merge([
+                          _animScaleCont,
+                          _animContSides,
                           _animContRot,
-                          _animScaleCont
                         ]),
                         builder: (context, child){
                           return Transform(
                               alignment: Alignment.center,
                               transform: Matrix4.identity()
-                                ..setRotationZ(_animRot.value)
-                                ..setRotationX(_animRot.value)
-                                ..setRotationY(_animRot.value)
-                                ..scale(_animScale.value),
+                                ..rotateZ(_animRot.value)
+                                ..rotateX(_animRot.value)
+                                ..rotateY(_animRot.value),
                               child: CustomPaint(
-                                size: const Size(350,350),
-                                painter: MyPainter(),
+                                painter: MyPainter(sides: _animSides.value),
+                                child: SizedBox(
+                                  height: _animScale.value,
+                                  width: _animScale.value
+                                )
                               )
                           );
                         }
@@ -95,19 +108,39 @@ class _StateCustomPaintClass extends State<CustomPaintClass> with TickerProvider
 }
 
 class MyPainter extends CustomPainter{
+  int sides;
+
+  MyPainter({
+    required this.sides
+});
+
   @override
   void paint(Canvas canvas, Size size){
     Paint paint = Paint();
     paint
     ..color = Colors.red
-    ..style = PaintingStyle.fill
+    ..style = PaintingStyle.stroke
     ..strokeWidth = 3;
 
 
     Path path = Path();
-    path.moveTo(size.width/6, size.height/6);
-    path.lineTo(size.width/2, size.height/2);
-    path.lineTo(size.width/6, size.height/2);
+    final center = Offset(size.width/2, size.width/2);
+    final angel = (2*pi)/sides;
+    final angels = List.generate(sides, (index)=>index*angel);
+    final radius = size.width/2;
+
+    path.moveTo(
+      center.dx + radius*cos(0),
+      center.dy + radius*sin(0)
+    );
+
+    for(double angle in angels){
+      path.lineTo(
+        center.dx + radius*cos(angle),
+        center.dy + radius*sin(angle)
+      );
+    }
+
     path.close();
 
     canvas.drawPath(path, paint);
